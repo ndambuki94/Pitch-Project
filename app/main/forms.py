45ad17c6287bcb51e1586dbe17d8c453407c8cc0
ndambuki94 <ndambuki94@gmail.com>
2019-10-24 20:12:42 +0300

    validation forms

--- app/main/forms.py
+++ app/main/forms.py
@@ -3,3 +3,25 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextA
 from wtforms.validators import Required, Email, EqualTo
 from ..models import User
 from wtforms import ValidationError
+
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('About you', validators=[Required()])
+    profile_pic_path = TextAreaField('Profile Picture URL', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+class ContentForm(FlaskForm):
+    content = TextAreaField('YOUR PITCH')
+    submit = SubmitField('SUBMIT')
+
+
+class CommentForm(FlaskForm):
+    comment_id = TextAreaField('WRITE COMMENT')
+    submit = SubmitField('SUBMIT')
+
+class PitchForm(FlaskForm): # creates a class that inherits from FlaskForm class
+    name = StringField('Authors Name', validators = [Required()])
+    categoy = TextAreaField('Pitch', validators=[Required()])
+    pitch = TextAreaField('Pitch', validators=[Required()])
+    submit = SubmitField('Submit')
