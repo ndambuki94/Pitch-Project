c65dc12bbbc831cdc63ea9ade5eaaf5e750c5ea8
ndambuki94 <ndambuki94@gmail.com>
2019-10-24 22:58:35 +0300

    api and secret key

---
+++ start.sh
@@ -0,0 +1,6 @@
+export MAIL_USERNAME=onesmusnzilani94@gmail.com
+export MAIL_PASSWORD=nzilani94
+export SECRET_KEY=nzilani94
+export DATABASE_URL='postgres://ldoyxgtsydhgpf:146955ca4ded7db5e6c408aeb59dbe561d1821297d9d81c7d83f045287c11b8b@ec2-54-225-103-255.compute-1.amazonaws.com:5432/ddfverud6cssdr'
+
+python3.6 manage.py server
