96dc4e3bb3768db98f3ef90b2c046b4b38fb3f09
ndambuki94 <ndambuki94@gmail.com>
2019-10-24 21:06:28 +0300

    import sqlachemy

--- migrations/eny.py
+++ migrations/eny.py
@@ -12,3 +12,12 @@ config = context.config
 # This line sets up loggers basically.
 fileConfig(config.config_file_name)
 logger = logging.getLogger('alembic.env')
+
+# here model's MetaData object
+# for 'autogenerate' support
+
+from flask import current_app
+config.set_main_option('sqlalchemy.url',
+                       current_app.config.get('SQLALCHEMY_DATABASE_URI'))
+target_metadata = current_app.extensions['migrate'].db.metadata
+
