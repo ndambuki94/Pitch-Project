62e8851b02e7fa17806816f4049309bbc88c9620
ndambuki94 <ndambuki94@gmail.com>
2019-10-24 14:56:52 +0300

    Create secret key

--- config.py
+++ config.py
@@ -1,4 +1,33 @@
 import os
 from flask import Flask
 
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:1234@localhost/pitches'
+    UPLOADED_PHOTOS_DEST ='app/static/images'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SECRET_KEY = 'nzilani94'
 
+    @staticmethod
+    def init_app(app):
+        pass
+
+class ProdConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:1234@localhost/pitches'
+    DEBUG = True
+
+class TestConfig(Config):
+    pass
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig,
+    }
